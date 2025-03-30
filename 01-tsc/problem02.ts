9ed44747aaf481b66c77f72fb1f2b2ed81fcd77f
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-03-31 00:56:07 +0530

    Doing some problems

---
+++ 01-tsc/problem02.ts
@@ -0,0 +1,7 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+let a: number = 5;
+let b: number = 6;
+console.log(sum(a, b));
---
+++ 01-tsc/problem03.ts
@@ -0,0 +1,6 @@
+function isLegal(age: number): boolean {
+    return age >= 18;
+}
+
+let age: number = 20;
+console.log(isLegal(age));
