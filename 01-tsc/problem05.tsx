14c8d7eccca4c96e31c02a05e9bf03e5606a2e24
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-03-31 12:28:49 +0530

    doing react in typescript already

---
+++ 01-tsc/problem05.tsx
@@ -0,0 +1,12 @@
+// todos.tsx
+// A react component
+
+// export default function App(): <Todo /> {
+//     return <Todo />
+// }
+
+// function Todo({}): <div /> {
+//     return (<div>
+//         Hi
+//     </div>)
+// }
